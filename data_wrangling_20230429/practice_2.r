69a81abce557bb2b06ea56a88d11e89a549507d4
tiwaomotosho <tiwababa194@gmail.com>
2023-05-03 13:57:10 +0100

    update for 2.1

--- data_wrangling_20230429/practice_2.r
+++ data_wrangling_20230429/practice_2.r
@@ -5,3 +5,16 @@ if (!require(tidyverse)) {
 }
 
 library(tidyverse)
+library(readxl)
+
+
+df1 <- read_excel("Production.xlsx", sheet = "Data")
+glimpse(df1)
+
+# Exploring the data content of line 2 - True header
+str(df1[2, ])
+
+# Removing Year from row 2 and promoting it to new header
+names(df1) <- str_replace_all(df1[2, ], "Year ", "")
+df1 <- df1[-c(1, 2), ]
+print(df1)
